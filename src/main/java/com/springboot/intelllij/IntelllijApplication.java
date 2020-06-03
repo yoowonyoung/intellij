e191af42ca1bcc461c4a340f3b478a82e66ae337
yoowonyoung <wonyoung0105@gmail.com>
2020-06-03 23:33:09 +0900

    init

---
+++ src/main/java/com/springboot/intelllij/IntelllijApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.intelllij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelllijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelllijApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/intelllij/IntelllijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.intelllij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelllijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
